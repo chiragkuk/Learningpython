190b97b860b87d97fb18b8b22451408c7e55b0ff
Chirag Kukreja <erkukrejachirag@gmail.com>
2023-05-05 03:33:48 +0530

    strings

---
+++ Chapter2/strings.py
@@ -0,0 +1,11 @@
+#Strings are collection of characters inside single quotes or double quotes
+
+firstName="Chirag"
+lastName="Kukreja"
+fullName=firstName+" " +lastName
+print(fullName)
+
+#Type error print(fullname + 3)
+
+print(fullName + str(3)) #Converts the Number to string alternaive is print(fullname + "3")
+print(fullName * 3) 
