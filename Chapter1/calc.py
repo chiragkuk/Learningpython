7b91466e45614f1b8be10fc6146126a2216031c1
Chirag Kukreja <erkukrejachirag@gmail.com>
2023-05-05 03:24:14 +0530

    New Programs

---
+++ Chapter1/calc.py
@@ -0,0 +1,10 @@
+print(2+3*4)
+#floating point division
+print(2/4)
+print(4/2)
+#Integer Division
+print(4//2)
+
+#Rounding 
+print(round(2**0.5, 4))
+
---
+++ Chapter1/variable.py
@@ -0,0 +1,12 @@
+number1 = 2
+print(number1)
+number1=4
+print(number1)
+name="Chirag"
+print(name)
+name=23
+print(name)
+####SNAKE CASE#### It is used in Python
+user_one_name="Mohit"
+####CAMEL CASE#### It is used in Java
+userOneName="Rohit"
